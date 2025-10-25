0130fca6098f8388e6cd7355fc0e461e6967c2e4
RiyaadHossain <riyadhossain.dev@gmail.com>
2025-10-25 15:52:26 +0600

    Added: MongoSanitize + Rate Limit

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,10 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 
 import { morganFormat, morganStream } from '@/configs/morgan.js';
+import { mongoSanitize } from '@/app/middlewares/mongo-sanitize.js';
+import configs from '@/configs/index.js';
+import { NODE_ENV_ENUM } from '@/enums/env.js';
+import { globalRateLimiter, speedLimiter } from '@/app/middlewares/rate-limit.js';
 
 const app: Application = express();
 
@@ -19,6 +23,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(morganFormat, { stream: morganStream }));
+app.use(mongoSanitize);
+
+// 🛡️ Apply rate limiting and speed limiting only in production
+if (configs.NODE_ENV === NODE_ENV_ENUM.PRODUCTION) {
+  app.use(globalRateLimiter);
+  app.use(speedLimiter);
+}
 
 // 🛣️ API Routes
 app.use('/api/v1', router);
